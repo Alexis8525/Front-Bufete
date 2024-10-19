5d637c31720afebccdbbd51af241f7a35c650a14
CristelRR <cristel23rr@gmail.com>
2024-10-18 19:36:03 -0600

    modificaciones sindebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatListModule} from '@angular/material/list'; 
-
-
-/** COMPONENTES */
 import { HomeComponent } from './components/home/home.component';
 import { SolicitudCitaComponent } from './components/solicitud-cita/solicitud-cita.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },        // Ruta por defecto (home)
@@ -23,20 +12,8 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [
-      RouterModule.forRoot(routes),
-      BrowserModule,
-      AppRoutingModule,
-      BrowserAnimationsModule,
-      MatIconModule,
-      MatButtonModule,
-      MatToolbarModule,
-      MatSidenavModule,
-      MatListModule
-    ],
-    exports: [
-      RouterModule,
-    ]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
   })
 
 export class AppRoutingModule { }
