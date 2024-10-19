5c863eaca0b7505edf6cf68fd2d8ba59ec6cf3c1
CristelRR <cristel23rr@gmail.com>
2024-10-18 19:29:25 -0600

    home y sindebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,42 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { HomeComponent } from './components/home/home.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list'; 
 
+
+/** COMPONENTES */
+import { HomeComponent } from './components/home/home.component';
+import { SolicitudCitaComponent } from './components/solicitud-cita/solicitud-cita.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },        // Ruta por defecto (home)
-  { path: 'home', component:HomeComponent }
+  { path: 'home', component:HomeComponent },
+  { path: 'cita', component: SolicitudCitaComponent},
+  { path: 'barra', component: SidebarComponent}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    imports: [
+      RouterModule.forRoot(routes),
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MatIconModule,
+      MatButtonModule,
+      MatToolbarModule,
+      MatSidenavModule,
+      MatListModule
+    ],
+    exports: [
+      RouterModule,
+    ]
   })
 
 export class AppRoutingModule { }
---
+++ src/app/components/solicitud-cita/solicitud-cita.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitud-cita',
+  standalone: true,
+  imports: [],
+  templateUrl: './solicitud-cita.component.html',
+  styleUrl: './solicitud-cita.component.css'
+})
+export class SolicitudCitaComponent {
+
+}
