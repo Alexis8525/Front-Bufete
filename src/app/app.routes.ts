1268cebf7e99e5d04893de2563f66e1d784a677a
CristelRR <cristel23rr@gmail.com>
2024-10-18 20:11:19 -0600

    Home elaborado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { SolicitudCitaComponent } from './components/solicitud-cita/solicitud-cita.component';
-import { BarraLateralComponent } from './components/barra-lateral/barra-lateral.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },        // Ruta por defecto (home)
   { path: 'home', component:HomeComponent },
   { path: 'cita', component: SolicitudCitaComponent},
-  { path: 'barra', component: BarraLateralComponent}
 ];
 
 @NgModule({
